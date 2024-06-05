e4ed4471354ec219b19ed4fdbc73f61560ec1f63
Br14nMat <1108638502@u.icesi.edu.co>
2024-06-05 16:35:09 -0500

    fix: token refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css';
-import Login from './components/Login/Login.tsx'
-import Home from './components/Home/Home.jsx'
-import Clients from './components/Clients/Clients.jsx'
-import Plans from './components/Plans/Plans.jsx'
-import Destinations from './components/Destinations/Destinations.jsx'
+import Login from './components/Login/Login.tsx';
+import Home from './components/Home/Home.jsx';
+import Clients from './components/Clients/Clients.jsx';
+import Plans from './components/Plans/Plans.jsx';
+import Destinations from './components/Destinations/Destinations.jsx';
 import { useState, useEffect } from "react";
-import { jwtDecode } from 'jwt-decode'
+import { jwtDecode } from 'jwt-decode'; // Cambiar esto
 import { CustomJwtPayload } from "./components/entity/CustomJwtPayload.tsx";
 import { getAuthToken } from "./components/services/BackendService.tsx";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import React from 'react'
+import React from 'react';
 import DestinationForm from './components/Destinations/DestinationForm.tsx';
 import PlanForm from './components/Plans/PlanForm.tsx';
 import ClientForm from './components/Clients/ClientForm.tsx';
@@ -23,24 +23,22 @@ export function App() {
   const [isAdmin, setIsAdmin] = useState(false);
   const [isSeller, setIsSeller] = useState(false);
   const [isUser, setIsUser] = useState(false);
+  const [token, setToken] = useState<string | null>(getAuthToken());
 
-  
   useEffect(() => {
-
-    let token = getAuthToken();
     if (token !== null) {
         setIsAuthenticated(true);
         const decoded = jwtDecode<CustomJwtPayload>(token);
         console.log(decoded);
         if (decoded.role === "ADMIN") {
             setIsAdmin(true);
-            setIsSeller(false)
-            setIsUser(false)
-        } else if (decoded.role === "SELLER"){
+            setIsSeller(false);
+            setIsUser(false);
+        } else if (decoded.role === "SELLER") {
           setIsSeller(true);
           setIsAdmin(false);
-          setIsUser(false)
-        }else if (decoded.role === "USER"){
+          setIsUser(false);
+        } else if (decoded.role === "USER") {
           setIsUser(true);
           setIsAdmin(false);
           setIsSeller(false);
@@ -48,27 +46,27 @@ export function App() {
     } else {
         setIsAuthenticated(false);
     }
+  }, [token]); // Dependencia en el token
 
-  }, []);
   return (
     <div>
       <Router>
         <Routes>
-          <Route path='/' element={isAuthenticated ? <Home/> : <Login />}/>
-          <Route path='/Login' element={<Login />}/>
-          <Route path='/Home' element={<Home />}/>
-          <Route path='/Clients' element={<Clients />}/>
-          <Route path='/Plans' element={ <Plans/>}/>
-          <Route path='/Destinations' element={<Destinations />}/>
+          <Route path='/' element={isAuthenticated ? <Home /> : <Login setToken={setToken} />} />
+          <Route path='/Login' element={<Login setToken={setToken} />} />
+          <Route path='/Home' element={<Home />} />
+          <Route path='/Clients' element={<Clients />} />
+          <Route path='/Plans' element={<Plans />} />
+          <Route path='/Destinations' element={<Destinations />} />
 
-          <Route path='/Clients/Create' element={isSeller || isAdmin ? <ClientForm/> : <Unauthorized />}/>
-          <Route path='/Clients/Edit/' element={isSeller || isAdmin ? <ClientEdit/> : <Unauthorized />}/>
-          <Route path='/Destinations/Edit/' element={isSeller || isAdmin ? <DestinationEdit/> : <Unauthorized />}/>
-          <Route path='/Plans/Edit/' element={isSeller || isAdmin ? <PlanEdit/> : <Unauthorized />}/>
-          <Route path='/Destinations/Create' element={isSeller || isAdmin ? <DestinationForm/> : <Unauthorized />}/>
-          <Route path='/Plans/Create' element={isSeller || isAdmin ? <PlanForm/> : <Unauthorized />}/>
+          <Route path='/Clients/Create' element={isSeller || isAdmin ? <ClientForm /> : <Unauthorized />} />
+          <Route path='/Clients/Edit/' element={isSeller || isAdmin ? <ClientEdit /> : <Unauthorized />} />
+          <Route path='/Destinations/Edit/' element={isSeller || isAdmin ? <DestinationEdit /> : <Unauthorized />} />
+          <Route path='/Plans/Edit/' element={isSeller || isAdmin ? <PlanEdit /> : <Unauthorized />} />
+          <Route path='/Destinations/Create' element={isSeller || isAdmin ? <DestinationForm /> : <Unauthorized />} />
+          <Route path='/Plans/Create' element={isSeller || isAdmin ? <PlanForm /> : <Unauthorized />} />
         </Routes>
-      </Router>
+      </Router>
     </div>
   );
 }
