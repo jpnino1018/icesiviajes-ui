753e998da853224b58f760edab436f1740c4a13f
Br14nMat <1108638502@u.icesi.edu.co>
2024-06-06 08:21:54 -0500

    fix: change delete btn location

--- src/components/Plans/PlanEdit.tsx
+++ src/components/Plans/PlanEdit.tsx
@@ -64,6 +64,25 @@ const onSubmit: SubmitHandler<any> = async (data) => {
   }
 };
 
+const handleDelete = async (idPlan) => {
+    const config = {
+      headers: {
+          Authorization: `Bearer ${getAuthToken()}`
+      }
+    }
+    
+    try {
+  
+        const response = await axios.delete(`http://localhost:9091/api/v1/plan/delete/${idPlan}`, config);
+  
+        alert(`Plan eliminado exitosamente`);
+        navigate('/Plans')
+  
+    } catch (error) {
+        console.error('Ocurrió un problema con la eliminación:', error);
+    }
+  };
+
 return (
 <div className='page'>
 <Navbar></Navbar>
@@ -125,7 +144,9 @@ return (
   </div>
 
   <div className='btn-container'>
-  <button className='send-form-btn' type="submit">Actualizar</button>
+    <button className='send-form-btn' type="submit">Actualizar</button>
+    <svg className='deleteBtn'style={{ margin: '1.5rem 2vh' }} onClick={() => handleDelete(planObj.idPlan)} xmlns="http://www.w3.org/2000/svg" height="50px" viewBox="0 -960 960 960" width="30px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
+
   </div>
 
   </form>
--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -54,7 +54,6 @@ import { useNavigate } from "react-router-dom";
               <div className='row-buttons'>
                 <svg className='checkBtn'xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFF"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Z"/></svg>
                 <svg className='editBtn' onClick={() => navigate('/Clients/Edit/', { state: {clientInfo: post}})} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
-                <svg className='deleteBtn' onClick={() => handleDelete(post[idKey])} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
               </div>
           </tr>
           ))}
@@ -81,7 +80,6 @@ import { useNavigate } from "react-router-dom";
               <div className='row-buttons'>
                 <svg className='checkBtn'xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFF"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Z"/></svg>
                 <svg className='editBtn' onClick={() => navigate('/Plans/Edit/', { state: {planInfo: post}})} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
-                <svg className='deleteBtn' onClick={() => handleDelete(post[idKey])} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
               </div>
           </tr>
           ))}
@@ -108,7 +106,6 @@ import { useNavigate } from "react-router-dom";
               <div className='row-buttons'>
                 <svg className='checkBtn'xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#FFF"><path d="M480-320q75 0 127.5-52.5T660-500q0-75-52.5-127.5T480-680q-75 0-127.5 52.5T300-500q0 75 52.5 127.5T480-320Zm0-72q-45 0-76.5-31.5T372-500q0-45 31.5-76.5T480-608q45 0 76.5 31.5T588-500q0 45-31.5 76.5T480-392Zm0 192q-146 0-266-81.5T40-500q54-137 174-218.5T480-800q146 0 266 81.5T920-500q-54 137-174 218.5T480-200Z"/></svg>
                 <svg className='editBtn' onClick={() => navigate('/Destinations/Edit/', { state: {destinationInfo: post}})} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M200-200h57l391-391-57-57-391 391v57Zm-80 80v-170l528-527q12-11 26.5-17t30.5-6q16 0 31 6t26 18l55 56q12 11 17.5 26t5.5 30q0 16-5.5 30.5T817-647L290-120H120Zm640-584-56-56 56 56Zm-141 85-28-29 57 57-29-28Z"/></svg>
-                <svg className='deleteBtn' onClick={() => handleDelete(post[idKey])} xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>
               </div>
           </tr>
           ))}
